4a07f7c10262559607c777f6167d4c7c407f9a96
granddevv <spacingflight@outlook.com>
2023-04-12 16:18:14 +0800

    add chart svg

---
+++ src/assets/charts/index.tsx
@@ -0,0 +1,250 @@
+export const ChartMuchBetter = () => (
+  <svg
+    width="117"
+    height="66"
+    viewBox="0 0 117 66"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="117" height="66" rx="4" fill="#F7F7F9" fill-opacity="0.8" />
+    <path
+      d="M16.5 31.5L100.5 31.5"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <line
+      x1="23"
+      y1="53.6"
+      x2="96.7854"
+      y2="11"
+      stroke="#24282F"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+    <path
+      d="M18.7444 43.3257L101.041 21.2743"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M23 11L96.7854 53.6"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M18.7443 21.2743L101.041 43.3257"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M60.0012 31.0002L64.1519 57.8475C64.2456 58.4534 63.7769 59.0002 63.1637 59.0002H56.8365C56.2233 59.0002 55.7545 58.4534 55.8483 57.8474L60.0012 31.0002Z"
+      fill="#D7DBE0"
+    />
+    <circle cx="59.8126" cy="32.0002" r="2.5" fill="white" stroke="#2A426F" />
+  </svg>
+);
+
+export const ChartSomewhatBetter = () => (
+  <svg
+    width="117"
+    height="66"
+    viewBox="0 0 117 66"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="117" height="66" rx="4" fill="#F7F7F9" fill-opacity="0.8" />
+    <path
+      d="M16.5 31.5L100.5 31.5"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M23 53.6L96.7854 11"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <line
+      x1={18.7444}
+      y1={43.3257}
+      x2={101.041}
+      y2={21.2743}
+      stroke="#24282F"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+    <path
+      d="M23 11L96.7854 53.6"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M18.7443 21.2743L101.041 43.3257"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M60.0012 31.0002L64.1519 57.8475C64.2456 58.4534 63.7769 59.0002 63.1637 59.0002H56.8365C56.2233 59.0002 55.7545 58.4534 55.8483 57.8474L60.0012 31.0002Z"
+      fill="#D7DBE0"
+    />
+    <circle cx="59.8126" cy="32.0002" r="2.5" fill="white" stroke="#2A426F" />
+  </svg>
+);
+
+export const ChartAboutSame = () => (
+  <svg
+    width="117"
+    height="66"
+    viewBox="0 0 117 66"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="117" height="66" rx="4" fill="#F7F7F9" fill-opacity="0.8" />
+
+    <path
+      d="M23 53.6L96.7854 11"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M18.7444 43.3257L101.041 21.2743"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M23 11L96.7854 53.6"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M18.7443 21.2743L101.041 43.3257"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <line
+      x1="17"
+      y1="31"
+      x2="100"
+      y2="31"
+      stroke="#24282F"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+    <path
+      d="M60.0012 31.0002L64.1519 57.8475C64.2456 58.4534 63.7769 59.0002 63.1637 59.0002H56.8365C56.2233 59.0002 55.7545 58.4534 55.8483 57.8474L60.0012 31.0002Z"
+      fill="#D7DBE0"
+    />
+    <circle cx="59.8126" cy="32.0002" r="2.5" fill="white" stroke="#2A426F" />
+  </svg>
+);
+
+export const ChartSomewhatWorse = () => (
+  <svg
+    width="117"
+    height="66"
+    viewBox="0 0 117 66"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="117" height="66" rx="4" fill="#F7F7F9" fill-opacity="0.8" />
+    <path
+      d="M16.5 31.5L100.5 31.5"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M23 53.6L96.7854 11"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M18.7444 43.3257L101.041 21.2743"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M23 11L96.7854 53.6"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <line
+      x1={18.7443}
+      y1={21.2743}
+      x2={101.041}
+      y2={43.3257}
+      stroke="#24282F"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+    <path
+      d="M60.0012 31.0002L64.1519 57.8475C64.2456 58.4534 63.7769 59.0002 63.1637 59.0002H56.8365C56.2233 59.0002 55.7545 58.4534 55.8483 57.8474L60.0012 31.0002Z"
+      fill="#D7DBE0"
+    />
+    <circle cx="59.8126" cy="32.0002" r="2.5" fill="white" stroke="#2A426F" />
+  </svg>
+);
+
+export const ChartGettingWorse = () => (
+  <svg
+    width="117"
+    height="66"
+    viewBox="0 0 117 66"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="117" height="66" rx="4" fill="#F7F7F9" fill-opacity="0.8" />
+    <path
+      d="M16.5 31.5L100.5 31.5"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M23 53.6L96.7854 11"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M18.7444 43.3257L101.041 21.2743"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <line
+      x1={23}
+      y1={11}
+      x2={96.7854}
+      y2={53.8}
+      stroke="#24282F"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+    <path
+      d="M18.7443 21.2743L101.041 43.3257"
+      stroke="#D7DBE0"
+      stroke-linecap="round"
+      stroke-dasharray="4 4"
+    />
+    <path
+      d="M60.0012 31.0002L64.1519 57.8475C64.2456 58.4534 63.7769 59.0002 63.1637 59.0002H56.8365C56.2233 59.0002 55.7545 58.4534 55.8483 57.8474L60.0012 31.0002Z"
+      fill="#D7DBE0"
+    />
+    <circle cx="59.8126" cy="32.0002" r="2.5" fill="white" stroke="#2A426F" />
+  </svg>
+);
